5653fc9855c711ade6494d5c0ee900e4376e552a
unknown <Dustin.Guillen14@yahoo.com>
2020-02-04 18:02:55 -0700

    added components

--- tictac/src/index.js
+++ tictac/src/index.js
@@ -1,12 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Square({ value, onClick }) {
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  return (
+    <button className="square" onClick={onClick}>
+      {value}
+    </button>
+  );
+}
+
+function Game() {
+
+    const [ squares, setSquares ] = useState(Array(9).fill('~'));
+
+    function RenderSquare(i) {
+        return <Square
+          value={squares[i]}
+          onClick={() => {
+            const nextSquares = squares.slice();
+              squares[i] = 'X';
+              setSquares(nextSquares)
+          }}
+        />;
+      }
+
+    return (
+      <div className="container">
+        <div className="game">
+          <div className="game-board">
+            <div className="board-row">
+              {RenderSquare(0)}
+              {RenderSquare(1)}
+              {RenderSquare(2)}
+            </div>
+            <div className="board-row">
+              {RenderSquare(3)}
+              {RenderSquare(4)}
+              {RenderSquare(5)}
+            </div>
+            <div className="board-row">
+              {RenderSquare(6)}
+              {RenderSquare(7)}
+              {RenderSquare(8)}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+ReactDOM.render(<Game />, document.getElementById("root"));
