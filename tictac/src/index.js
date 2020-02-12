9ed5ddd18ed640818a86c2baf8ddc3d72e1263a9
unknown <Dustin.Guillen14@yahoo.com>
2020-02-11 17:48:49 -0700

    added dependency react-router-dom

--- tictac/src/index.js
+++ tictac/src/index.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+
 import "./index.css";
 import App from "./App.js";
 
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Router><App /></Router>, document.getElementById("root"));
