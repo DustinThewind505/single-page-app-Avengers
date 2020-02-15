5e4fc6641cc7d81bf9e54c33da908e7ba315cc07
unknown <Dustin.Guillen14@yahoo.com>
2020-02-15 15:56:17 -0700

    added more fonts and an event listener on the home page

--- tictac/src/components/Home.js
+++ tictac/src/components/Home.js
@@ -4,7 +4,12 @@ function Home() {
 
     return (
         <div className="home">
-            <img src="https://comicvine1.cbsistatic.com/uploads/original/11/117763/2598672-marvel_zombies___dead_days___page_5.jpg"/>
+            <h2>Home</h2>
+            <img onMouseOver={event => event.target.src="https://comicvine1.cbsistatic.com/uploads/original/11/117763/2598672-marvel_zombies___dead_days___page_5.jpg"} 
+            onMouseLeave={event => event.target.src="http://getwallpapers.com/wallpaper/full/9/4/b/1191411-marvel-avengers-desktop-wallpaper-1920x1080-for-android-40.jpg"}
+            src="http://getwallpapers.com/wallpaper/full/9/4/b/1191411-marvel-avengers-desktop-wallpaper-1920x1080-for-android-40.jpg"/>
+            <h1>Home</h1>
+            
         </div>
     )
 }
