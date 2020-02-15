44b022307b0d509d825e7996557aae0d962ae4b9
unknown <Dustin.Guillen14@yahoo.com>
2020-02-14 19:55:46 -0700

    styling for home page

--- tictac/src/components/Home.js
+++ tictac/src/components/Home.js
@@ -4,7 +4,6 @@ function Home() {
 
     return (
         <div className="home">
-            <h2>Home</h2>
             <img src="https://comicvine1.cbsistatic.com/uploads/original/11/117763/2598672-marvel_zombies___dead_days___page_5.jpg"/>
         </div>
     )
