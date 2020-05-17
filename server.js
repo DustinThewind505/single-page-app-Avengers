21e34caeb4155571fbfe4ff21191b8a0036f31b4
unknown <Dustin.Guillen14@yahoo.com>
2020-05-16 22:53:10 -0600

    added server.js and gitignore node

---
+++ server.js
@@ -0,0 +1,14 @@
+const http : require('http');
+
+const PORT = 420;
+
+const server = http.createServer((req, res) => {
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text-plain');
+    res.end("I love Toasty");
+})
+
+server.listen(PORT, () => {
+    console.log(`\n*** Server is running on localhost/${PORT} ***\n`)
+});
