ee5ad25eed7f8598eea702d7ba30651c35d65ae9
unknown <Dustin.Guillen14@yahoo.com>
2020-09-01 12:34:07 -0600

    New home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,18 @@ function Home() {
   return (
     <div className="home">
       <img
+        // onMouseOver={event =>
+        //   (event.target.src =
+        //     "https://66.media.tumblr.com/fb2a20c08246e0102e10198ec60988c5/tumblr_oopx94VQTR1v6a1bfo1_400.gif")
+        // }
+        // onMouseLeave={event =>
+        //   (event.target.src =
+        //     "http://getwallpapers.com/wallpaper/full/9/4/b/1191411-marvel-avengers-desktop-wallpaper-1920x1080-for-android-40.jpg")
+        // }
+        src="https://66.media.tumblr.com/fb2a20c08246e0102e10198ec60988c5/tumblr_oopx94VQTR1v6a1bfo1_400.gif"
+        alt="Avengers assembling"
+      />
+      <img
         onMouseOver={event =>
           (event.target.src =
             "https://comicvine1.cbsistatic.com/uploads/original/11/117763/2598672-marvel_zombies___dead_days___page_5.jpg")
@@ -15,7 +27,6 @@ function Home() {
         src="http://getwallpapers.com/wallpaper/full/9/4/b/1191411-marvel-avengers-desktop-wallpaper-1920x1080-for-android-40.jpg"
         alt="Avengers assembling"
       />
-      <h1>Home</h1>
     </div>
   );
 }
