70375fa3b78f6408cc9922c934acd487b484528b
unknown <Dustin.Guillen14@yahoo.com>
2024-02-25 10:28:26 -0700

    [ticket-number] ran 'npm audit fix' to update node modules, switched home and footer images to local files

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,13 +10,13 @@ function Footer() {
         <>
             <footer>
                 <div className='footer-left'>
-                    <img src='https://thefangirlproject.com/wp-content/uploads/2014/05/marvellogothing.png' alt='zombie logo' />
+                    <img src='images/hydra.png' alt='zombie logo' />
                 </div>
                 <div className='footer-middle'>
-                    <img src='http://2.bp.blogspot.com/-CfcNNFkQgKg/VVA2-IZrYMI/AAAAAAAACW4/2UKFilGssf0/s1600/Marvel-comics-logo-vector.png' alt='zombie logo' />
+                    <img src='images/marvel-logo.png' alt='zombie logo' />
                 </div>
                 <div className='footer-right'>
-                    <img src='https://thefangirlproject.com/wp-content/uploads/2014/05/marvellogothing.png' alt='zombie logo' />
+                    <img src='images/hydra.png' alt='zombie logo' />
                 </div>
             </footer>
         </>
--- src/components/Home.js
+++ src/components/Home.js
@@ -12,24 +12,24 @@ function Home(props) {
     <div className="home-container">
       <div className="home-header">
         <img
-          src="https://66.media.tumblr.com/fb2a20c08246e0102e10198ec60988c5/tumblr_oopx94VQTR1v6a1bfo1_400.gif"
+          src="images/marvel-gif.gif"
           alt="Avengers assembling"
         />
         <img
           onMouseOver={event =>
             (event.target.src =
-              "https://i.pinimg.com/736x/8f/7e/8b/8f7e8b2248d6a9a2def7730fc7da2c1c.jpg")
+              "images/avengers1978.jpg")
           }
           onMouseLeave={event =>
             (event.target.src =
-              "http://majorspoilers.com/wp-content/uploads/2012/10/MarvelZombiesHalloween.jpg")
+              "images/avengers-halloween.jpg")
           }
-          src="http://majorspoilers.com/wp-content/uploads/2012/10/MarvelZombiesHalloween.jpg"
+          src="images/avengers-halloween.jpg"
           alt="Avengers assembling"
         />
       </div>
       {props.data.map(avenger => (
-        <div className="home-accordian" onClick={() => handleSingleselection(avenger.id)}>
+        <div className="home-accordian" onClick={() => handleSingleselection(avenger.id)} key={avenger.id}>
           <h2>{avenger.name}</h2>
           <span>+</span>
           {singleAccordianSelecion === avenger.id ?<p>{avenger.description}</p> : null}
