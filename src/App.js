9ab88b4db7530ed8bf63f19c8743cae89a3c3ba7
unknown <Dustin.Guillen14@yahoo.com>
2024-03-01 12:44:52 -0700

    [ticket-number] setup axios call to GET data from api, deleted axios call from avengerslist.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Route, Link } from "react-router-dom";
 
 import Home from "./components/Home";
@@ -7,8 +7,22 @@ import Avenger from "./components/Avenger";
 import Footer from './components/Footer';
 
 import Data from "./Data";
+import axios from "axios";
 
 function App() {
+  const [avengersData, setAvengersData] = useState([])
+
+
+  useEffect(() => {
+    axios.get(`http://localhost:8000/avengers`)
+    .then(response => {
+      setAvengersData(response.data);
+    })
+    .catch(err => {
+      console.log('Axios Error', err)
+    })
+  }, [])
+
   return (
     <div className="App">
       <ul className="navbar">
@@ -22,16 +36,16 @@ function App() {
       <Route 
         exact 
         path="/" 
-        render={props => <Home {...props} data={Data} />}
+        render={props => <Home {...props} data={avengersData ? Data : avengersData} />}
       />
       <Route
         exact
         path="/avengers"
-        render={props => <AvengersList {...props} data={Data} />}
+        render={props => <AvengersList {...props} data={avengersData ? Data : avengersData} />}
       />
       <Route
         path="/avengers/:id"
-        render={props => <Avenger {...props} data={Data} />}
+        render={props => <Avenger {...props} data={avengersData ? Data : avengersData} />}
       />
       <Footer />
     </div>
--- src/components/AvengersList.js
+++ src/components/AvengersList.js
@@ -1,32 +1,11 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React from "react";
 
 
 function AvengersList(props) {
 
-  const [test123, setTest123] = useState()
 
-  // var test123 
-
-  // Wont connect with localhost http://localhost:3005/anasazi, needs IP adress to work http://127.0.0.1:3000/anasazi
-  axios.get(`http://127.0.0.1:3000/anasazi`)
-  .then(response => {
-    console.log(response.data)
-    setTest123(response.data);
-    console.log(typeof test123)
-  })
-  .catch(err => {
-    console.log('There is an error ----->', err)
-  })
-  // const response = fetch("http://localhost:3005/anasazi");
-  // console.log(response)
-
-
-  console.log(test123)
   return (
     <div className="characters-list-wrapper">
-      {test123}
-      <p>TEST!@#</p>
       {props.data.map(hero => (
         <div
           className="character-card"
