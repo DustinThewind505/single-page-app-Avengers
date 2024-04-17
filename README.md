fd2237b13b8a2c10a987f559f759d25502c0568e
Dustin G <Dustin.Guillen14@yahoo.com>
2024-04-16 22:17:58 -0600

    [ticket-number] added README, began accordian component on characters page

---
+++ README.md
@@ -0,0 +1,18 @@
+# Welcome to my Avengers app! (React)
+
+To run:
+### `npm start`
+
+## Home Page
+
+
+
+## Characters Page
+
+
+Characters page has accordian components
+
+## React
+To learn React, check out the [React documentation](https://reactjs.org/).
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/components/AvengersList.js
+++ src/components/AvengersList.js
@@ -1,12 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 function AvengersList(props) {
+  const [singleAccordianSelection, setSingleAccordianSelection] = useState("villians");
 
 
   return (
     <div className="characters-list-wrapper">
-      {props.data.map(hero => (
+      <div  onClick={() => setSingleAccordianSelection("heros")}>
+        <h2>Heros</h2>
+          {
+            singleAccordianSelection === "heros" ?
+              props.data.map(hero => (
+                <div
+                  className="character-card"
+                  key={hero.id}
+                  onClick={() => props.history.push(`/avengers/${hero.id}/details`)}
+                >
+                  <img src={hero.thumbnail} alt={hero.name} />
+                  <h3>{hero.name}</h3>
+                  <h4>({hero.nickname})</h4>
+                </div>
+              ))
+              :
+              null
+          }
+      </div>
+      <div  onClick={() => setSingleAccordianSelection("villians")}>
+        <h2>Villians</h2>
+        {
+            singleAccordianSelection === "villians" ?
+              props.data.map(hero => (
+                <div
+                  className="character-card"
+                  key={hero.id}
+                  onClick={() => props.history.push(`/avengers/${hero.id}/details`)}
+                >
+                  <img src={hero.thumbnail} alt={hero.name} />
+                  <h3>{hero.name}</h3>
+                  <h4>({hero.nickname})</h4>
+                </div>
+              ))
+              :
+              null
+          }
+      </div>
+      
+
+      {/* {props.data.map(hero => (
         <div
           className="character-card"
           key={hero.id}
@@ -16,7 +57,7 @@ function AvengersList(props) {
           <h3>{hero.name}</h3>
           <h4>({hero.nickname})</h4>
         </div>
-      ))}
+      ))} */}
     </div>
   );
 }
